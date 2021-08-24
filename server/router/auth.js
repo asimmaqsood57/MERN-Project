ba5a3b9b50a03729acfa5a251b3d3dc576eb6de1
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-24 18:55:44 +0500

    async await added

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,41 +9,78 @@ router.get("/", (req, res) => {
   res.send("Hello from the server router");
 });
 
-router.post("/register", (req, res) => {
+//using promises
+
+// router.post("/register",  (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(420).json({ error: "please fill it again" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(420).json({ error: "User already exits" });
+//       }
+//     })
+//     .catch((err) => {
+//       res.send("there is an error in validation", err);
+//     });
+
+//   const user = new User({
+//     name: name,
+//     email: email,
+//     phone: phone,
+//     work: work,
+//     password: password,
+//     cpassword: password,
+//   });
+
+//   user
+//     .save()
+//     .then(() => {
+//       res.status(201).send("data is inserted");
+//     })
+//     .catch((err) => {
+//       res.status(500).send("there is and error");
+//       console.log(err);
+//     });
+// });
+
+//using async await
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(420).json({ error: "please fill it again" });
   }
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(420).json({ error: "User already exits" });
-      }
-    })
-    .catch((err) => {
-      res.send("there is an error in validation", err);
+  try {
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.status(420).json({ error: "User already exits" });
+    }
+
+    const user = new User({
+      name: name,
+      email: email,
+      phone: phone,
+      work: work,
+      password: password,
+      cpassword: password,
     });
 
-  const user = new User({
-    name: name,
-    email: email,
-    phone: phone,
-    work: work,
-    password: password,
-    cpassword: password,
-  });
+    const userRegister = await user.save();
 
-  user
-    .save()
-    .then(() => {
+    if (userRegister) {
       res.status(201).send("data is inserted");
-    })
-    .catch((err) => {
-      res.status(500).send("there is and error");
-      console.log(err);
-    });
+    }
+  } catch (error) {
+    res.status(500).send("there is and error");
+    console.log(error);
+  }
 });
 
 module.exports = router;
