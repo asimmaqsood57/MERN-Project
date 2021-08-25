a7e1ad70d146c7a1256fb557d9d96a58f398ea87
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-25 07:56:53 +0500

    token is strored in cookies

--- server/router/auth.js
+++ server/router/auth.js
@@ -108,6 +108,12 @@ router.post("/login", async (req, res) => {
 
     console.log(token);
 
+    res.cookie("jwtoken", token, {
+      expires: new Date(Date.now() + 25892000000),
+
+      httpOnly: true,
+    });
+
     if (!isMatch) {
       res.json({ message: "Invalid credentials" });
     } else {
